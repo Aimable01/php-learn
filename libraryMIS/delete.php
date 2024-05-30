925e97f40380b25d7e1cfd761a208c8cc9a9054d
Aimable01 <aimable.kwizera14@gmail.com>
2024-05-30 10:45:32 +0200

    delete db

---
+++ libraryMIS/delete.php
@@ -0,0 +1,13 @@
+<?php
+include 'database.php';
+
+$id = $_GET['id'];
+
+$sql = "DELETE FROM logbook_entries WHERE id=$id";
+if ($conn->query($sql) === TRUE) {
+    header("Location: index.php");
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
